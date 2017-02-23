f2951b8281978cbb2762b6ae515a7e56def2ec2d
xdkxlk <xdkxlk@outlook.com>
2017-02-23 22:27:11 +0800

    项目初始化

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+///<reference path="../../typings/index.d.ts"/>
+
+import {Component, AfterViewInit} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit {
+
   title = 'app works!';
+
+  constructor() {
+  }
+
+  ngAfterViewInit(): void {
+    $(".carousel").carousel();
+  }
+
 }
